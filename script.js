6c99d3078bb52bbf77916c69592bf52a63166092
laurajunco <lc.junco10@uniandes.edu.co>
2018-11-29 16:38:59 -0500

    fix repeating images

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
   var height = $( window ).height();
 
   var topMargin = $(".top-menu").height() + 50;
-  var numImagenes = 51;
-  var cuenta = 1;
+  var numImagenes = 50;
+  var cuenta = 0;
   var r = 100;
 
   setInterval(addImg, 300);
@@ -13,7 +13,10 @@ $(document).ready(function(){
   function addImg() {
 
     //solo mientras se cargan las imagenes
-    if ( cuenta < numImagenes ) {
+    if (cuenta < numImagenes ) {
+
+       //aumenta la cuenta
+       cuenta++;
 
       var src = "img/" + cuenta + ".png"
 
@@ -22,42 +25,41 @@ $(document).ready(function(){
 
       //cuando carga la imagen
       img.onload = function() {
-        var imgWidth = this.width;
-        var imgHeight = this.height;
+        if (cuenta < numImagenes ) {
 
-        //crear div y obtener dimensiones
-         var div = $("<div> </div>")
-         var img = $("<img/>");
-         var marco = $("<div> </div>");
+          var imgWidth = this.width;
+          var imgHeight = this.height;
 
-         img.attr("src",src);
-         marco.addClass("marco-imagen");
+          //crear div y obtener dimensiones
+           var div = $("<div> </div>")
+           var img = $("<img/>");
+           var marco = $("<div> </div>");
 
-         //dividir tamano
-         var r = 1.1 + (Math.random() * 3.5);
-         div.width(imgWidth / r);
-         div.height((imgHeight / r) + 20);
+           img.attr("src",src);
+           marco.addClass("marco-imagen");
 
-
-         //variables de posicion aleatoria
-         var top =  topMargin + Math.floor(Math.random() * (height - topMargin - div.height()));
-
-         var left =  Math.floor(Math.random() * (width - div.width()));
-
-         //agregar classes y definir posicion
-         div.addClass("imagen-archivo");
-         div.addClass("drag");
-         div.css({top: top, left: left});
-
-         div.append(marco);
-         div.append(img);
-
-         //agregar el elemento al documento
-         $("body").append(div);
+           //dividir tamano
+           var r = 1.1 + (Math.random() * 3.5);
+           div.width(imgWidth / r);
+           div.height((imgHeight / r) + 20);
 
 
-         //aumenta la cuenta
-         cuenta++;
+           //variables de posicion aleatoria
+           var top =  topMargin + Math.floor(Math.random() * (height - topMargin - div.height()));
+
+           var left =  Math.floor(Math.random() * (width - div.width()));
+
+           //agregar classes y definir posicion
+           div.addClass("imagen-archivo");
+           div.addClass("drag");
+           div.css({top: top, left: left});
+
+           div.append(marco);
+           div.append(img);
+
+           //agregar el elemento al documento
+           $("body").append(div);
+         }
       }
 
       //activar draggable
